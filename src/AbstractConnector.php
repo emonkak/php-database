549eb9d4411fda5f6d8e18002a3d95851759464a
Shota Nozaki <emonkak@gmail.com>
2016-11-19 21:08:19 +0900

    Refactor PDOConnector

---
+++ src/AbstractConnector.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace Emonkak\Database;
+
+/**
+ * Represents a database connection that can be connected/disconnected.
+ */
+abstract class AbstractConnector implements PDOInterface
+{
+    /**
+     * @var PDOInterface
+     */
+    private $pdo;
+
+    /**
+     * Gets the PDO connection of the database. And connect to the database if
+     * not connected yet.
+     *
+     * @return PDOInterface
+     */
+    public function getPdo()
+    {
+        if ($this->pdo === null) {
+            $this->pdo = $this->doConnect();
+        }
+        return $this->pdo;
+    }
+
+    /**
+     * Returns whether the connection is available.
+     *
+     * @return boolean
+     */
+    public function isConnected()
+    {
+        return $this->pdo !== null;
+    }
+
+    /**
+     * Disconnects the connection.
+     */
+    public function disconnect()
+    {
+        if ($this->pdo !== null) {
+            $this->doDisconnect($this->pdo);
+            $this->pdo = null;
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function beginTransaction()
+    {
+        return $this->getPdo()->beginTransaction();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function commit()
+    {
+        return $this->getPdo()->commit();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function errorCode()
+    {
+        return $this->getPdo()->errorCode();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function errorInfo()
+    {
+        return $this->getPdo()->errorInfo();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function exec($statement)
+    {
+        return $this->getPdo()->exec($statement);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function inTransaction()
+    {
+        return $this->getPdo()->inTransaction();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function lastInsertId($name = null)
+    {
+        return $this->getPdo()->lastInsertId();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function prepare($statement)
+    {
+        return $this->getPdo()->prepare($statement);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function query($statement, $param1 = null, $param2 = null, $param3 = null)
+    {
+        return $this->getPdo()->query($statement, $param1, $param2, $param3);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function quote($string, $parameter_type = null)
+    {
+        return $this->getPdo()->quote($string, $parameter_type);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rollback()
+    {
+        return $this->getPdo()->rollback();
+    }
+
+    /**
+     * Connects the connection.
+     *
+     * @return PDOInterface
+     */
+    abstract protected function doConnect();
+
+    /**
+     * Disconnects the connection.
+     *
+     * @param PDOInterface $pdo
+     */
+    abstract protected function doDisconnect(PDOInterface $pdo);
+}
--- src/PDOConnector.php
+++ src/PDOConnector.php
@@ -2,10 +2,7 @@
 
 namespace Emonkak\Database;
 
-/**
- * The serializable implementation of PDOInterface by PDO.
- */
-class PDOConnector implements PDOInterface
+class PDOConnector extends AbstractConnector
 {
     /**
      * @var string
@@ -28,11 +25,6 @@ class PDOConnector implements PDOInterface
     private $options;
 
     /**
-     * @var \PDO
-     */
-    private $pdo;
-
-    /**
      * @param string      $dsn
      * @param string|null $user
      * @param string|null $password
@@ -46,128 +38,23 @@ class PDOConnector implements PDOInterface
         $this->options = $options;
     }
 
-    /**
-     * Gets the PDO connection of the database. And connect to the database if
-     * not connected yet.
-     *
-     * @return PDO
-     */
-    public function getPdo()
-    {
-        if ($this->pdo === null) {
-            $this->pdo = new PDO($this->dsn, $this->user, $this->password, $this->options);
-        }
-        return $this->pdo;
-    }
-
-    /**
-     * Returns whether the connection is available.
-     *
-     * @return boolean
-     */
-    public function isConnected()
-    {
-        return !!$this->pdo;
-    }
-
-    /**
-     * Disconnects this connection.
-     */
-    public function disconnect()
-    {
-        $this->pdo = null;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function beginTransaction()
-    {
-        return $this->getPdo()->beginTransaction();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function commit()
-    {
-        return $this->getPdo()->commit();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function errorCode()
-    {
-        return $this->getPdo()->errorCode();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function errorInfo()
-    {
-        return $this->getPdo()->errorInfo();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function exec($statement)
-    {
-        return $this->getPdo()->exec($statement);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function inTransaction()
-    {
-        return $this->getPdo()->inTransaction();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function lastInsertId($name = null)
-    {
-        return $this->getPdo()->lastInsertId();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function prepare($statement)
-    {
-        return $this->getPdo()->prepare($statement);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function query($statement, $param1 = null, $param2 = null, $param3 = null)
-    {
-        return $this->getPdo()->query($statement, $param1, $param2, $param3);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function quote($string, $parameter_type = null)
-    {
-        return $this->getPdo()->quote($string, $parameter_type);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function rollback()
-    {
-        return $this->getPdo()->rollback();
-    }
-
     public function __sleep()
     {
         return array('dsn', 'user', 'password', 'options');
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function doConnect()
+    {
+        return new PDO($this->dsn, $this->user, $this->password, $this->options);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function doDisconnect(PDOInterface $pdo)
+    {
     }
 }
