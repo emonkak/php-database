8a0235df964136b8ffa6dfa822ad596f8f847a6e
Shota Nozaki <emonkak@gmail.com>
2024-05-16 15:57:10 +0900

    test: add coverage ListenableStatement::execute()

--- tests/ListenableStatementTest.php
+++ tests/ListenableStatementTest.php
@@ -78,6 +78,38 @@ class ListenableStatementTest extends TestCase
             ->with(
                 $this->identicalTo($pdo),
                 $queryString,
+                [123],
+                $this->greaterThan(0)
+            );
+        $delegate = $this->createMock(PDOStatementInterface::class);
+        $delegate
+            ->expects($this->once())
+            ->method('bindValue')
+            ->with(1, 123, \PDO::PARAM_INT)
+            ->willReturn(true);
+        $delegate
+            ->expects($this->once())
+            ->method('execute')
+            ->willReturn(true);
+
+        $stmt = new ListenableStatement($pdo, [$listener], $delegate, $queryString);
+
+        $this->assertTrue($stmt->bindValue(1, 123, \PDO::PARAM_INT));
+        $this->assertTrue($stmt->execute([]));
+    }
+
+    public function testExecuteWithParams(): void
+    {
+        $queryString = 'SELECT ? AS foo, ? AS bar';
+
+        $pdo = $this->createMock(PDOInterface::class);
+        $listener = $this->createMock(PDOListenerInterface::class);
+        $listener
+            ->expects($this->once())
+            ->method('onQuery')
+            ->with(
+                $this->identicalTo($pdo),
+                $queryString,
                 [123, 456],
                 $this->greaterThan(0)
             );
